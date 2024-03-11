fc62fb934adf7f6f21328414df052297cb0d9d99
max9147 <73356484+max9147@users.noreply.github.com>
2024-03-11 14:24:13 +0400

    video selection base

--- Assets/Scripts/MainScreenContentConroller.cs
+++ Assets/Scripts/MainScreenContentConroller.cs
@@ -2,13 +2,19 @@ using DG.Tweening;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainScreenContentConroller : MonoBehaviour
 {
     private const int ID = 0;
 
     [SerializeField] private CameraController _cameraController;
+    [SerializeField] private GameObject _selectionCanvasLeft;
+    [SerializeField] private GameObject _selectionCanvasRight;
+    [SerializeField] private RawImage _video;
     [SerializeField] private TextMeshPro _mainScreenText;
+    [SerializeField] private VideoPlayer _videoPlayer;
 
     private Coroutine _startingHoverCoroutine;
     private Coroutine _stoppingHoverCoroutine;
@@ -59,7 +65,8 @@ public class MainScreenContentConroller : MonoBehaviour
     {
         if (_currentPoint == ID)
         {
-
+            StopAllCoroutines();
+            StartCoroutine(PlayingVideo());
         }
     }
 
@@ -67,7 +74,8 @@ public class MainScreenContentConroller : MonoBehaviour
     {
         if (_currentPoint == ID)
         {
-
+            StopAllCoroutines();
+            StartCoroutine(HidingVideo());
         }
     }
 
@@ -77,6 +85,7 @@ public class MainScreenContentConroller : MonoBehaviour
         DOTween.To(() => _quickOutline.OutlineWidth, x => _quickOutline.OutlineWidth = x, 10f, 0.5f);
 
         _mainScreenText.DOColor(Color.white, 0.5f);
+        DOTween.To(() => _mainScreenText.fontMaterial.GetFloat(ShaderUtilities.ID_GlowPower), x => _mainScreenText.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, x), 1f, 0.5f);
 
         yield return new WaitForSeconds(0.5f);
     }
@@ -86,9 +95,44 @@ public class MainScreenContentConroller : MonoBehaviour
         DOTween.To(() => _quickOutline.OutlineWidth, x => _quickOutline.OutlineWidth = x, 0f, 0.5f);
 
         _mainScreenText.DOColor(new Color(1f, 1f, 1f, 0f), 0.5f);
+        DOTween.To(() => _mainScreenText.fontMaterial.GetFloat(ShaderUtilities.ID_GlowPower), x => _mainScreenText.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, x), 0f, 0.5f);
 
         yield return new WaitForSeconds(0.5f);
 
         _quickOutline.enabled = false;
+    }
+
+    private IEnumerator PlayingVideo()
+    {
+        yield return new WaitForSeconds(1f);
+
+        _selectionCanvasLeft.transform.localPosition = new Vector3(1f, 1f, 1.35f);
+        _selectionCanvasRight.transform.localPosition = new Vector3(-1f, 1f, 1.35f);
+
+        _selectionCanvasLeft.SetActive(true);
+        _selectionCanvasRight.SetActive(true);
+
+        _selectionCanvasLeft.transform.DOLocalMove(new Vector3(2.3f, -1f, 1.35f), 1f).SetEase(Ease.InOutSine);
+        _selectionCanvasRight.transform.DOLocalMove(new Vector3(-2.3f, -1f, 1.35f), 1f).SetEase(Ease.InOutSine);
+
+        _video.DOColor(Color.white, 0.5f);
+        _videoPlayer.Play();
+    }
+
+    private IEnumerator HidingVideo()
+    {
+        _video.DOColor(new Color(1f, 1f, 1f, 0f), 0.5f);
+
+        _selectionCanvasLeft.transform.DOLocalMove(new Vector3(1f, 1f, 1.35f), 1f).SetEase(Ease.InOutSine);
+        _selectionCanvasRight.transform.DOLocalMove(new Vector3(-1f, 1f, 1.35f), 1f).SetEase(Ease.InOutSine);
+
+        yield return new WaitForSeconds(0.5f);
+
+        _videoPlayer.Stop();
+
+        yield return new WaitForSeconds(0.5f);
+
+        _selectionCanvasLeft.SetActive(false);
+        _selectionCanvasRight.SetActive(false);
     }
 }
